e061469b3945ff62d6a745be7b2622501d7f8ca1
SaiyemBejoy <jsbejoy96@gmail.com>
2020-05-08 00:43:17 +0600

    Initial deply on TomCat as war file

--- src/main/java/com/example/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/example/app/ws/MobileAppWsApplication.java
@@ -2,13 +2,20 @@ package com.example.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.app.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
@@ -18,13 +25,13 @@ public class MobileAppWsApplication {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
-	
-	@Bean(name="AppProperties")
+
+	@Bean(name = "AppProperties")
 	public AppProperties getAppProperties() {
 		return new AppProperties();
 	}
